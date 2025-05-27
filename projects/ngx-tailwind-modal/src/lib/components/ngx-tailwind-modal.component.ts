a2d320af57ade1f246db1389bb4a6175ec1ae4f8
Luis M Dev <lm.torresrivera@gmail.com>
2025-05-27 14:35:31 +0200

    chore: bump version to 0.0.6 and enhance modal data assignment
    - Updated package version from 0.0.5 to 0.0.6 in package.json.
    - Enhanced modal data assignment in NgxTailwindModalComponent to include identifier and modal instance.

--- projects/ngx-tailwind-modal/src/lib/components/ngx-tailwind-modal.component.ts
+++ projects/ngx-tailwind-modal/src/lib/components/ngx-tailwind-modal.component.ts
@@ -348,7 +348,7 @@ export class NgxTailwindModalComponent implements OnInit, OnDestroy, AfterViewCh
    */
   private assignModalDataToComponentData(componentRef: ComponentRef<Component>): void {
     if (componentRef) {
-      Object.assign(componentRef.instance, this._data);
+      Object.assign(componentRef.instance, { id: this.identifier, modalInstance: this, ...this._data });
     }
   }
 
